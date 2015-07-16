847083a4dc6756dffcf490c97ff5efb9bbc63405
yonh <azssjli@163.com>
2015-07-16 14:52:20 +0800

    fix bug

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-useradd ssh
+useradd sshd
 p=`cat /dev/urandom | tr -dc "a-zA-Z0-9_+\~\!\@\#\$\%\^\&\*\(\)"| fold -w 12 |head -n 1` 
-echo -e "$p\n$p" |passwd ssh && echo $p
+echo -e "$p\n$p" |passwd sshd && echo $p
 
 /usr/sbin/sshd -D
